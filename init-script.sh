adcb79056a48c9bbba29265ebeafe4bcaaa2e152
obabaldbiyat <127845320+obabaldbiyat@users.noreply.github.com>
2023-05-29 14:18:40 +0200

    Update init-script.sh

--- init-script.sh
+++ init-script.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-#Installing Docker
-...
-...
+#Installing Docker 
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+sudo apt install docker-ce
 
-#Creating container
-...
-...
+#Creating container 
+sudo docker run -d -p 80:80 obab/nginx-personalised-with-index.html:latest
